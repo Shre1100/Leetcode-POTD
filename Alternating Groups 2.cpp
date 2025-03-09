bead3dd7e27c2e0a9ac90926b75afa8d3e12324e
Shreya Kumar <148473866+Shre1100@users.noreply.github.com>
2025-03-09 16:44:07 +0530

    Create Alternating Groups II

---
+++ Alternating Groups 2.cpp
@@ -0,0 +1,24 @@
+// Problem Statement--> There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i].
+// colors[i] == 0 means that tile i is red.
+// colors[i] == 1 means that tile i is blue.
+// An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+// Return the number of alternating groups.
+
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+        int count = 0;
+        int left = 0;
+        for (int right = 0; right < n + k - 1; ++right) {
+            if (right > 0 && colors[right % n] == colors[(right - 1) % n]) {
+                left = right;  
+            }
+            
+            if (right - left + 1 >= k) {
+                count++;  
+            }
+        }
+        return count;
+    }
+};

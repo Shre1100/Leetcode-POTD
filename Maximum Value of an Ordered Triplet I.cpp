297be9181173df447ab98a5c0427a04149ef4b93
Shreya Kumar <148473866+Shre1100@users.noreply.github.com>
2025-04-02 22:22:25 +0530

    Create Maximum Value of an Ordered Triplet I

---
+++ Maximum Value of an Ordered Triplet I.cpp
@@ -0,0 +1,16 @@
+// You are given a 0-indexed integer array nums.
+// Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+// The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long maxTriplet = 0, maxElement = 0, maxDiff = 0;
+        for (long num : nums) {
+            maxTriplet = max(maxTriplet, maxDiff * num);
+            maxDiff = max(maxDiff, maxElement - num);
+            maxElement = max(maxElement, num);
+        }
+        return maxTriplet;
+    }
+};
